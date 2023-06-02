e6b9e801dbe8a79aa23d0d7744fcfb7d60298c89
cheranchintucct <cheranchintu0@gmail.com>
2023-06-02 20:19:02 +0100

    Made model work. Needs integration into server

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,15 @@
 from flask import Flask, request, jsonify
 
+import sys
+import os
+# Get the path to the data directory relative to this module
+data_path = os.path.join(os.path.dirname(__file__), '..', 'spam_detection_model')
+
+# Add the data directory to sys.path
+sys.path.append(data_path)
+
+from spam_detection_model.model import models_response
+
 app = Flask(__name__)
 
 @app.route("/members")
@@ -11,8 +21,13 @@ def get_input():
     if request.method == "POST":
 
         reqContent = request.get_json()
+        if models_response([reqContent]):
+            ret = "It is a spam message"
+        else:
+            ret = "It is not a spam message"
         print(reqContent)
-        return jsonify({"conversion": "Sample"})
+
+        return jsonify({"conversion": ret})
 
 if __name__ == "__main__" :
     app.run(debug = True)
--- spam_detection_model/model.py
+++ spam_detection_model/model.py
@@ -1,16 +1,9 @@
 import numpy as np 
-import pandas as pd 
-from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
 import pickle
 
 def models_response(message = ""):
-    message = np.array(message.split())
-    message.reshape(1,-1)
-    model1 = pickle.load(open("spam_detection_model/multinomialnb.pkl", "rb"))
-    model2 = pickle.load(open("spam_detection_model/support-vector-machine.pkl", "rb"))
-    model3 = pickle.load(open("spam_detection_model/randomforestclassifier.pkl", "rb"))
-    return [model1.predict(message), model1.predict(message), model1.predict(message)]
+    model =  pickle.load(open("spam_detection_model/clfPipeline.pkl", "rb"))
+    return True if model.predict([message]) == np.array([1]) else False
